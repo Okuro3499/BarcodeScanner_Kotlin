9b6a041fcafb665cca2c6d15d8e4b9bfa7517da5
Okuro3499 <33450715Go>
2022-06-27 15:07:19 +0300

    create a barcode and qr code scanner

--- app/src/main/java/com/extrainch/qrbarcodescanner/MainActivity.kt
+++ app/src/main/java/com/extrainch/qrbarcodescanner/MainActivity.kt
@@ -1,135 +1,120 @@
-package com.extrainch.qrbarcodescanner;
+package com.extrainch.qrbarcodescanner
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.media.AudioManager;
-import android.media.ToneGenerator;
-import android.os.Bundle;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.media.ToneGenerator
+import android.os.Bundle
+import android.view.SurfaceHolder
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.extrainch.qrbarcodescanner.databinding.ActivityMainBinding
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.Detector.Detections
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import com.extrainch.qrbarcodescanner.databinding.ActivityMainBinding;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+    private var barcodeDetector: BarcodeDetector? = null
+    private var cameraSource: CameraSource? = null
+    private var toneGen1: ToneGenerator? = null
+    private var barcodeData: String? = null
 
-import java.io.IOException;
-
-public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
-    private BarcodeDetector barcodeDetector;
-    private CameraSource cameraSource;
-    private static final int REQUEST_CAMERA_PERMISSION = 201;
-    private ToneGenerator toneGen1;
-    private String barcodeData;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-        initialiseDetectorsAndSources();
-
-//        binding.Scan.setOnClickListener(v -> scanCode());
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+        initialiseDetectorsAndSources()
     }
 
-    private void initialiseDetectorsAndSources() {
-        //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+    private fun initialiseDetectorsAndSources() {
+        barcodeDetector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
 
-        barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.ALL_FORMATS)
-                .build();
+        cameraSource = CameraSource.Builder(this, barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
 
-        cameraSource = new CameraSource.Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(1920, 1080)
-                .setAutoFocusEnabled(true) //you should add this feature
-                .build();
-
-        binding.surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
+        binding!!.surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
                 try {
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                        cameraSource.start(binding.surfaceView.getHolder());
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        cameraSource?.start(binding!!.surfaceView.holder)
                     } else {
-                        ActivityCompat.requestPermissions(MainActivity.this, new
-                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.CAMERA),
+                            REQUEST_CAMERA_PERMISSION
+                        )
                     }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                } catch (e: IOException) {
+                    e.printStackTrace()
                 }
             }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
             }
 
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource?.stop()
             }
-        });
+        })
 
 
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
+        barcodeDetector?.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
                 // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
             }
 
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+            override fun receiveDetections(detections: Detections<Barcode>) {
+                val barcodes = detections.detectedItems
                 if (barcodes.size() != 0) {
-
-
-                    binding.barcodeText.post(new Runnable() {
-
-                        @Override
-                        public void run() {
-
-                            if (barcodes.valueAt(0).email != null) {
-                                binding.barcodeText.removeCallbacks(null);
-                                barcodeData = barcodes.valueAt(0).email.address;
-                                binding.barcodeText.setText(barcodeData);
-                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
-                            } else {
-
-                                barcodeData = barcodes.valueAt(0).displayValue;
-                                binding.barcodeText.setText(barcodeData);
-                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
-
-                            }
+                    binding!!.barcodeText.post {
+                        if (barcodes.valueAt(0).email != null) {
+                            binding!!.barcodeText.removeCallbacks(null)
+                            barcodeData = barcodes.valueAt(0).email.address
+                            binding!!.barcodeText.text = barcodeData
+                            toneGen1!!.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+                        } else {
+                            barcodeData = barcodes.valueAt(0).displayValue
+                            binding!!.barcodeText.text = barcodeData
+                            toneGen1!!.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
                         }
-                    });
-
+                    }
                 }
             }
-        });
+        })
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        getSupportActionBar().hide();
-        cameraSource.release();
+    override fun onPause() {
+        super.onPause()
+        supportActionBar!!.hide()
+        cameraSource!!.release()
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getSupportActionBar().hide();
-        initialiseDetectorsAndSources();
+    override fun onResume() {
+        super.onResume()
+        supportActionBar!!.hide()
+        initialiseDetectorsAndSources()
     }
 
-//    private void scanCode() {
-//        IntentIntegrator intergrator
-//    }
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 201
+    }
 }
